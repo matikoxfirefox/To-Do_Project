90559c64d7dd34defe2802f5a98decba47b9c7fe
matikoxfirefox <ffox77343@gmail.com>
2025-12-26 23:19:10 +0100

    Connection with DataBase

--- Backend/Appsettings/Program.cs
+++ Backend/Appsettings/Program.cs
@@ -1,7 +1,16 @@
 using Backend.Models;
-
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql("Host=localhost;Port=5432;Database=ToDoDb;Username=postgres;Password=Maskotis123"));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,40 +23,41 @@ builder.Services.AddCors(options =>
             .AllowAnyMethod();
     });
 });
+
 var app = builder.Build();
 
 app.UseCors("AllowAll");
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
 
+var todos = new List<ToDoItem>();
 
-var todos = new List<ToDoItem>
+app.MapGet("/api/todos", async (AppDbContext db) =>
 {
-   // new ToDoItem { Id = 1, Title = "Zrobic backend"},
-   // new ToDoItem { Id = 2, Title = "Odpalic Reacta"},
-};
-
-app.MapGet("/api/todos", () => todos);
-app.MapPost("/api/todos", (ToDoItem newTodo) =>
-{
-    todos.Add(newTodo);
-    return newTodo;
+    return await db.Todos.ToListAsync();
 });
-app.MapPut("/api/todos/{id}", (int id, ToDoItem updatedTodo) =>
+app.MapPost("/api/todos", async (AppDbContext db, ToDoItem newTodo) =>
 {
-    var todo = todos.Find(t => t.Id == id);
-    if (todo == null) 
-        return Results.NotFound("Not found");
-
+    db.Todos.Add(newTodo);
+    await db.SaveChangesAsync();
+    return Results.Created($"/api/todos/{newTodo.Id}", newTodo);
+});
+app.MapPut("/api/todos/{id}", async (AppDbContext db, int id, ToDoItem updatedTodo) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+    if (todo == null) return Results.NotFound("Not found");
     todo.Title = updatedTodo.Title;
     todo.IsDone = updatedTodo.IsDone;
-
-    return Results.Ok(todo); 
+    await db.SaveChangesAsync();
+    return Results.Ok(todo);
 });
-app.MapDelete("/api/todos/{id}", (int id) =>
+app.MapDelete("/api/todos/{id}", async (AppDbContext db, int id) =>
 {
-    var todo = todos.Find(t => t.Id == id);
-    if (todo == null)
-        return Results.NotFound("Not found");
-    todos.Remove(todo);
+    var todo = await db.Todos.FindAsync(id);
+    if (todo == null) return Results.NotFound("Not found");
+    db.Todos.Remove(todo);
+    await db.SaveChangesAsync();
     return Results.Ok(todo);
 });
 
@@ -59,29 +69,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
