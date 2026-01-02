27fc94fe8356e0baf1a9e272e81782028a50b4a6
matikoxfirefox <ffox77343@gmail.com>
2026-01-02 14:00:29 +0100

    rgfereg

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Groups from "./components/Groups";
 import ToDoList from "./components/ToDoList";
 import Contact from "./components/Contact";
 import About from "./components/About.jsx";
-const API = "http://localhost:5263/api";
+const API = "https://untethered-supererogatory-elsa.ngrok-free.dev/";
 
 function App() {
     const [userId, setUserId] = useState(() => {
